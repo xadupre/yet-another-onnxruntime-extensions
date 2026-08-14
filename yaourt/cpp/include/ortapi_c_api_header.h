e8236b442c3e82bd71861e692c3eb523cf75f36e
Xavier Dupré <xadupre@microsoft.com>
2026-08-14 08:18:29 +0200

    update version

--- yaourt/cpp/include/ortapi_c_api_header.h
+++ yaourt/cpp/include/ortapi_c_api_header.h
@@ -12,13 +12,7 @@
 
 #else
 
-#define IS_EMPTY(x) IS_EMPTY_HELPER(x)
-#define IS_EMPTY_HELPER(x) IS_EMPTY_CHECK(x ## 1, 1)
-#define IS_EMPTY_CHECK(a, b, ...) b
-
-#if IS_EMPTY(ORT_EXPORT)
 #undef ORT_EXPORT
-#define ORT_EXPORT __attribute__ ((visibility("default")))
-#endif
+#define ORT_EXPORT __attribute__((visibility("default")))
 
 #endif
